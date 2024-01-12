7f48340e1a98f98790250d9712d3500eba6157ff
John Scott <j@sciencehike.com>
2024-01-12 00:01:33 -0700

    improve L3 video and text

--- contents/lemma3/lemma-conf.js
+++ contents/lemma3/lemma-conf.js
@@ -12,14 +12,14 @@
             sappCodeReference : 'lemma2',
             "contents-list" :
             [
-                "claim-latin.content.txt",
-                "claim-mix.content.txt",
-                "claim-xixcentury.content.txt",
-                "proof-mix.content.txt",
-                "proof-xixcentury.content.txt",
-                "proof-xix-century-II.content.txt",
-                "proof-xix-century-II.svg.txt",
-                "proof-xix-century-III.content.txt",
+                "txt/latin.txt",
+                "txt/cohen.txt",
+                "txt/video.txt",
+                "txt/claim-xixcentury.content.txt",
+                "txt/proof-xixcentury.content.txt",
+                "txt/proof-xix-century-II.content.txt",
+                "txt/proof-xix-century-II.svg.txt",
+                "txt/proof-xix-century-III.content.txt",
             ],
 
         };
