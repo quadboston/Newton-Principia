c65426e72295917711c0a33e466cf5a70ba8d699
John Scott <j@sciencehike.com>
2024-11-23 19:50:42 -0700

    Correct for version number going "backwards"

--- version.js
+++ version.js
@@ -3,7 +3,7 @@
     var fapp        = ns.fapp           = ns.fapp           || {};
 
     // //\\ updated automatically. Don't edit these strings.
-    fapp.version =  13023; //application version
+    fapp.version =  14000; //application version
     // \\// updated automatically. Don't edit these strings.
 
 }) ();
