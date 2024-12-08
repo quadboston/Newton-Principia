7a0b6bff868ec2ce9fa1a7f8116be9de18e183d5
John Scott <j@sciencehike.com>
2024-12-07 21:37:40 -0700

    Tweak model height threshold

--- contents/conf/conf.js
+++ contents/conf/conf.js
@@ -69,7 +69,7 @@
 
         //.below this value, JS considers the device as a mobile
         MOBILE_MEDIA_QUERY_WIDTH_THRESHOLD : 800,
-        MOBILE_MEDIA_QUERY_HEIGHT_THRESHOLD : 800,
+        MOBILE_MEDIA_QUERY_HEIGHT_THRESHOLD : 600,
 
         model_float_dir : 'right', //vs left
         ESSAY_PANE_IS_BEFORE_MEDIA_IN_HTML : true,
