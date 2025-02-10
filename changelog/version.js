b2a9b5e83df56cd8de407d00af5941e16acbfe3b
Konstantin Kirillov <github@landkey.net>
2025-02-09 19:45:47 -0600

    13340-ver-T-L2-popup

--- changelog/version.js
+++ changelog/version.js
@@ -3,8 +3,8 @@
     var fapp        = ns.fapp           = ns.fapp           || {};
 
     // //\\ updated automatically. Don't edit these strings.
-    fapp.version = 13338; //application version
-    fapp.buildDateString = "2025-02-07"; //build date
+    fapp.version = 13342; //application version
+    fapp.buildDateString = "2025-02-09"; //build date
     // \\// updated automatically. Don't edit these strings.
 
     }) ();
--- contents/lemma2/js/init-model-parameters.js
+++ contents/lemma2/js/init-model-parameters.js
@@ -37,6 +37,38 @@
     ///one of the first thing called in app-wide sapp.init_sapp
     function init_model_parameters()
     {
+
+        {
+            let pid = 'proof-pop-up';
+            let button = ssF.createButton({
+                caption                 :
+                    'Newton\'s logic applies to monotonic curves',
+                buttonUniversalId       : pid,
+                //scenarioEventOnClick    : 'graph-is-plotted',
+                clickCallback           : () => {
+                    sData[ pid ].dom$.css( 'display', 'none' );
+                },
+                noTopicScenario         : true,
+                cssText                 : `
+                    position            : absolute;
+                    width               : 330px;
+                    height              : 18px;
+                    top                 : 0%;
+                    padding             : 8px;
+                    left                : 50%;
+                    transform           : translate( -50%, 0% );
+                    border-radius       : 15px;
+                    border              : 3px outset #cccccc;
+                    font-size           : 16px;
+                    text-align          : center;
+                    background-color    : #dddddd;
+                    color               : red;
+                    cursor              : pointer;
+                    z-index             : 111111111;
+                `,
+            });
+        }        
+
         // dom z-order patch
         haff( ssF, 'continue_create_8_prepopulate_svg' );
         
@@ -94,6 +126,7 @@
         //createsBaseSlider();
 
         gui.buildSlider();
+        
         sDomF.detected_user_interaction_effect( 'doUndetected' );
     }
 
