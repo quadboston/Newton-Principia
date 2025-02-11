5cf0ae0190845d7c7d09b3f1cfa1f7252b534da5
Konstantin Kirillov <github@landkey.net>
2025-02-11 01:20:05 -0600

    13347-ver-bug-p6-Invalid-Orbit-Detection:NON_SOLVABLE_THRESHOLD=0.05:

--- changelog/version.js
+++ changelog/version.js
@@ -3,7 +3,7 @@
     var fapp        = ns.fapp           = ns.fapp           || {};
 
     // //\\ updated automatically. Don't edit these strings.
-    fapp.version = 13347; //application version
+    fapp.version = 13348; //application version
     fapp.buildDateString = "2025-02-11"; //build date
     // \\// updated automatically. Don't edit these strings.
 
