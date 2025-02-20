a999ad8422e8939bc842b9609b0abd6396ba4471
Sarah Rosanna Busch <annarose0113@gmail.com>
2025-02-20 12:21:29 -0800

    combining nav-bar-and-drawer, checkbox, tabs, essaion-pane, menu-on-left into lemma-comp.css

--- src/base/css/dispatch-lemma-css.js
+++ src/base/css/dispatch-lemma-css.js
@@ -1,17 +1,12 @@
 ( function() {
     var {
         ns, sn, cssp, engCssMs,
-        fconf,
+        fconf, sDomN 
     } = window.b$l.apptree({
     });
     engCssMs.places_engineCSSes2styleTags = places_engineCSSes2styleTags;
+    engCssMs['menu-on-left'] = createsMenuCss;
     return;
-
-
-
-
-
-
 
     ///no not site wide ... not for home page
     ///only for lemmas/lessons
@@ -22,11 +17,7 @@
 
         //data-entry: put module names here in order
         `
-            nav-bar-and-drawer
-            checkbox
-            tabs
             dom-subroots
-            essaion-pane
             menu-on-left
         `
         .split(/\r\n|\n/g)
@@ -58,5 +49,51 @@
     ******************************************/
     `;
     }
+
+    function createsMenuCss( cssp, fconf ) {
+        var theorionChildWidth = (100 / sDomN.theorionMenuMembersCount).toFixed();
+        var aspectionChildWidth = (100 / sDomN.aspectionMenuMembersCount).toFixed();
+        var leftTopLeafLength =
+            ( sDomN.aspectionMenuMembersCount * fconf.LEFT_SIDE_MENU_ITEM_LENGTH ).toFixed();
+
+        let root = document.documentElement;
+        root.style.setProperty('--theorionChildWidth', theorionChildWidth);
+        root.style.setProperty('--aspectionChildWidth', aspectionChildWidth);
+        root.style.setProperty('--leftTopLeafLength', leftTopLeafLength);
+
+        //===========================
+        // //\\ shuttle 
+        //===========================
+        // //\\ setting up shuttle CSS for all possible menu leaf choices
+        var ret;
+        for( var ix=0; ix<sDomN.theorionMenuMembersCount; ix++ ) {
+            ret += `
+                .menu-teaf.theorion .decorated.litem-${ix},
+                .menu-teaf.theorion .litem-${ix},
+                .menu-teaf.theorion .shuttle-${ix} {
+                    left       :${theorionChildWidth*ix}%;
+                }
+                .menu-teaf.theorion .shuttle-${ix} {
+                    transition :top 0.3s ease-in-out, left 0.5s ease-in-out;
+                }
+            `;
+        }
+
+        for( var ix=0; ix<sDomN.aspectionMenuMembersCount; ix++ ) {
+            ret += `
+                .menu-teaf.aspect .decorated.litem-${ix},
+                .menu-teaf.aspect .litem-${ix},
+                .menu-teaf.aspect .shuttle-${ix} {
+                    left       :${aspectionChildWidth*ix}%;
+                }
+                .menu-teaf.aspect .shuttle-${ix} {
+                    transition :top 0.3s ease-in-out, left 0.5s ease-in-out;
+                }
+            `;
+        }
+
+        return ret;
+    }
+    
 }) ();
 
