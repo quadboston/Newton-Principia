9f1a6dda542920578705c417154f50e81ab95800
Sarah Rosanna Busch <annarose0113@gmail.com>
2025-03-05 15:37:51 -0800

    Refactor CSS strings to external CSS, with 3 universal font families; serif and box for quoted text
    * homepage.css replaces home-pane.css.js and homepage-basics.css.js
    
    * roots.css - all css vars defined here
    
    * reset.css.js and typography.css.js combined into roots.css
    
    * inner-page.css.js to inner-page.css
    
    * dispatch-home-css.js now obsolete...code cleanup still needed
    
    * combining nav-bar-and-drawer, checkbox, tabs, essaion-pane, menu-on-left into lemma-comp.css
    
    * subroots.css.js to subroots.css
    
    * site.css.js added to roots.css
    
    * dispatch-css.js now handles small amount of css that still requires js to set up
    
    * moving all css to /src/base/css, deleting obsolete files
    
    * page specific (lemma2 etc) redundant inner-page, slider, model .css.js's combined into .css under src/base/css/pages
    
    * src/vendor/bsl/gui/local-content-rtigger/trigger.css.js styles moved to src/base/css/inner-page.css
    
    * universal font-families, 3 categories - default: serif, quoted text: sans, numbers: mono
    
    * fonts refactor
    
    * font refactored
    
    * code cleanup to complete css refactor
    
    * Google font, quoted text box styles

---
+++ src/base/css/dispatch-css.js
@@ -0,0 +1,99 @@
+( function() {
+    var {
+        ns, sn, cssp, engCssMs,
+        sconf, fconf, sDomN 
+    } = window.b$l.apptree({
+    });
+    const root = document.documentElement;
+    engCssMs.dispatches_css = dispatch_css; //called from app-main.js to init variable css
+    engCssMs['menu-on-left'] = createsMenuCss;
+    return;
+
+    function dispatch_css() 
+    {
+        //ensures css vars are correct
+        root.style.setProperty('--main_horizontal_dividor_width_px', sconf.main_horizontal_dividor_width_px + "px");
+        root.style.setProperty('--doDisplayPageTopNavigatMenu', fconf.doDisplayPageTopNavigatMenu ? 'flex' : 'none');
+        root.style.setProperty('--LEFT_SIDE_MENU_WIDTH', fconf.LEFT_SIDE_MENU_WIDTH + "px");
+        root.style.setProperty('--LEFT_SIDE_MENU_OFFSET_X', fconf.LEFT_SIDE_MENU_OFFSET_X + "px");
+
+        //data-entry: put module names here in order
+        `
+            menu-on-left
+        `
+        .split(/\r\n|\n/g)
+        .forEach( function( modname ) {
+            modname = modname.replace(/\s+/g,'');
+            if( modname ) {
+                ////this list is prone to manual-errors, commas, etc
+                ////this is why this debug-style-coding
+                var cssGenerator = engCssMs[ modname ];
+                var cssTxt = cssGenerator( cssp, fconf );
+                //update( moreText, htmlkey )
+                ns.globalCss.update( //addText(
+                    decorateText( cssTxt, modname ),
+                    'lemma-style'
+                );
+            }
+        });
+    }
+
+    function decorateText( text, modname )
+    {
+        return ` 
+    /******************************************
+       //\\\\ css lemma module = ${modname}
+    ******************************************/
+    ${text}        
+    /******************************************
+       \\\\// css lemma module = ${modname}
+    ******************************************/
+    `;
+    }
+
+    /* text area horizontal and vertical tab buttons */
+    function createsMenuCss( cssp, fconf ) {
+        var theorionChildWidth = (100 / sDomN.theorionMenuMembersCount).toFixed();
+        var aspectionChildWidth = (100 / sDomN.aspectionMenuMembersCount).toFixed();
+        var leftTopLeafLength =
+            ( sDomN.aspectionMenuMembersCount * fconf.LEFT_SIDE_MENU_ITEM_LENGTH ).toFixed();
+
+        root.style.setProperty('--theorionChildWidth', theorionChildWidth);
+        root.style.setProperty('--aspectionChildWidth', aspectionChildWidth);
+        root.style.setProperty('--leftTopLeafLength', leftTopLeafLength);
+
+        //===========================
+        // //\\ shuttle 
+        //===========================
+        // //\\ setting up shuttle CSS for all possible menu leaf choices
+        var ret;
+        for( var ix=0; ix<sDomN.theorionMenuMembersCount; ix++ ) {
+            ret += `
+                .menu-teaf.theorion .decorated.litem-${ix},
+                .menu-teaf.theorion .litem-${ix},
+                .menu-teaf.theorion .shuttle-${ix} {
+                    left       :${theorionChildWidth*ix}%;
+                }
+                .menu-teaf.theorion .shuttle-${ix} {
+                    transition :top 0.3s ease-in-out, left 0.5s ease-in-out;
+                }
+            `;
+        }
+
+        for( var ix=0; ix<sDomN.aspectionMenuMembersCount; ix++ ) {
+            ret += `
+                .menu-teaf.aspect .decorated.litem-${ix},
+                .menu-teaf.aspect .litem-${ix},
+                .menu-teaf.aspect .shuttle-${ix} {
+                    left       :${aspectionChildWidth*ix}%;
+                }
+                .menu-teaf.aspect .shuttle-${ix} {
+                    transition :top 0.3s ease-in-out, left 0.5s ease-in-out;
+                }
+            `;
+        }
+
+        return ret;
+    }
+
+}) ();
