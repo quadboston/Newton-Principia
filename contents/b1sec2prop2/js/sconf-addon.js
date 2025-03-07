67bd11679c4751f6b34a13886f60b501899dbd38
John Scott <j@sciencehike.com>
2025-03-07 16:45:49 -0700

    Change P2 to Cohen's translation (#21)
    Also, highlights and link updated.

--- contents/b1sec2prop2/js/sconf-addon.js
+++ contents/b1sec2prop2/js/sconf-addon.js
@@ -138,12 +138,12 @@
             "Caracc"            : [100,   0,  100],
             "CCaracc"           : [100,   0,  100],
             "CParacc"           : [100,   0,  100],
-            "SBCaracc"          : [255,  100, 0, 0.5],
+            "SBCaracc"          : [255,  100, 0, 0.5],  // triangle SBC area description accelerated
             "cCaracc"           : [100,   0,  100],
 
             "Varacc"            : forceMoveX,
-            "BVaracc"           : forceMoveX,
-            "VVaracc"           : forceMoveX,
+            "BVaracc"           : forceMoveX,   // force at B deviated toward V; area description accelerated
+            "VVaracc"           : forceMoveX,   // force vector at V displaced to V'; area description accelerated
             "CParacc"           : forceMoveX,
             "CaraccParacc"      : [100,   0,  100],
 
--- contents/b1sec2prop2/lemma-conf.js
+++ contents/b1sec2prop2/lemma-conf.js
@@ -57,7 +57,7 @@
             "contents-list" :
             [
                 './txt/latin.txt',
-                './txt/english.txt',
+                './txt/cohen.txt',
                 './txt/addendum-comment.txt',
             ],
             //optional additional reference html
