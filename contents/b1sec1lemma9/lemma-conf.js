ecbe5d581260f121461d6eef8386b919deb32e9b
John Scott <j@sciencehike.com>
2025-03-12 16:18:40 -0600

    L9: Polish text

--- contents/b1sec1lemma9/lemma-conf.js
+++ contents/b1sec1lemma9/lemma-conf.js
@@ -16,27 +16,11 @@
         "contents-list" :
         [
             'txt/latin.txt',
-            'txt/english.txt',
+            'txt/cohen.txt',
             'txt/video.txt',
         ],
         //optional additional reference html
-        referencesForAllLemmaEssays : `
-            <br><br>
-            Sources:
-            <br>
-            <a href="https://www.jstor.org/stable/10.1525/j.ctt9qh28z" target="_blank">
-                3rd Edition English translation by I. Bernard Cohen.
-            </a>
-            <br>
-            <a href="https://www.e-rara.ch/zut/wihibe/content/pageview/338107">
-                Lemma 9 Latin text and figure. 3rd Edition.
-            </a>
-        `,
-
-
-
-
-
+        referencesForAllLemmaEssays : ``,
     }; }
 }) ();
 
