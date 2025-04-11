3b5e917fc69e28c4c2814b59e278acfdbb1a3620
Konstantin Kirillov <github@landkey.net>
2025-04-11 09:50:47 -0500

    P7. Makes second center R user-movable in corollary 2.

--- changelog/version.js
+++ changelog/version.js
@@ -3,8 +3,8 @@
     var fapp        = ns.fapp           = ns.fapp           || {};
 
     // //\\ updated automatically. Don't edit these strings.
-    fapp.version = 13546; //application version
-    fapp.buildDateString = "2025-04-09"; //build date
+    fapp.version = 13547; //application version
+    fapp.buildDateString = "2025-04-10"; //build date
     // \\// updated automatically. Don't edit these strings.
 
     }) ();
