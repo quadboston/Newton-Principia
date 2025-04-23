ffa2f354d95df4cc90d0c0212723eb64f833aee9
David-J-McMurtrie <david.mcmurtrie@gmail.com>
2025-04-23 10:48:49 -0400

    Switching model to use global color constants
    Switching model to use global color constants

--- contents/b1sec2prop10/js/sconf.js
+++ contents/b1sec2prop10/js/sconf.js
@@ -1,20 +1,8 @@
 
 ( function() {
-    var { //import from apptree
-        ns,
-        fconf,
-        sconf,
-    } = window.b$l.apptree({ //export to apptree
-        ssFExportList : { init_conf }
-    });
+    var { ns, fconf, sconf, fixedColors, } = 
+        window.b$l.apptree({ ssFExportList : { init_conf } });
     return;
-
-
-
-
-
-
-
 
 
     //====================================================
@@ -123,14 +111,19 @@
         // //\\ topic group colors,
         //      todm: possibly proliferation
         //-----------------------------------
-        var given   = [0,     150, 0,      1];
-        var proof   = [0,     0,   255,    1];
-        var result  = [200,   40,  0,      1];
-        var curvature  = [200,   40,  200, 1];
-        var body    = [0,     150,  200,   1];
-        var hidden  = [0,     0,   0,      0];
-        var context = [0,     0,   0,      1];
-        var invalid = [200,  150,  0,      1];
+        const {
+            given,
+            body,
+            orbit,
+            proof,
+            force,
+            result,
+            hidden,
+            curvature,
+            context,
+        } = fixedColors;
+
+
         var predefinedTopics =
         {
             given,
@@ -140,8 +133,8 @@
             context,
             curvature,
             body,
-            orbit   : given,
-            force   : result,
+            orbit,
+            force,
             tangentCircle : curvature,
             //curvatureCircle : curvature,
         };
