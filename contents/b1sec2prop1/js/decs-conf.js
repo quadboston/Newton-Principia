f06b240fd786ca88934605c205e2da36ecf700f2
David-J-McMurtrie <david.mcmurtrie@gmail.com>
2025-04-25 09:01:15 -0400

    Removed extra shapes, points, lines
    -Removed extra shapes “SABCD" and "SABCDEF”.  They were previously used for highlighting however the needed triangles already exist, and it’s best practice not to have multiple pieces of code that are intended to do the same thing.
    -Removed duplicate points ‘h’, ‘g’ and related lines.
    -Add comments to make searching easier

--- contents/b1sec2prop1/js/decs-conf.js
+++ contents/b1sec2prop1/js/decs-conf.js
@@ -1,27 +1,11 @@
 ( function() {
-    var {
-        sn, eachprop, mapp, nspaste, haz, has,
-        sconf, toreg, rg, ssF, ssD, sDomF,
-        amode, stdMod,
-    } = window.b$l.apptree({
-        stdModExportList :
-        {
-            decShapes_conf,
-        },
-    });
+    var { sn, eachprop, mapp, nspaste, haz, has, sconf, toreg, rg, ssF, ssD, sDomF, amode, 
+        stdMod, } = window.b$l.apptree({ stdModExportList : { decShapes_conf, }, });
+
     var decor = sn( 'decor', stdMod );
     
     var LOGIC = false; //true; //makes logic steps c and C clearer;
     return;
-
-
-
-
-
-
-
-
-
 
 
     //----------------------------------------
@@ -118,21 +102,6 @@
                     cssClass : 'theor1proof theor2proof theor2corollary',
             },
             // \\// c,d,e,f
-
-            // //\\ c-col3
-            h   : {
-                caption : 'c',
-                decStart : decor.C.decStart+1,
-                decEnd : decor.F.decStart+4,
-                cssClass : 'theor1corollary theor2proof',
-            },
-            g   : {
-                caption : 'f',
-                decStart : decor.F.decStart,
-                decEnd : decor.F.decEnd,
-                cssClass : 'theor1corollary',
-            },
-            // \\// c-col3
         };
 
         var forceTip_conf = {
@@ -339,10 +308,6 @@
                 cssClass : 'theor1proof theor2proof',
                 decEnd : rg.f.decStart,
             },
-            { nam : ['B', 'h'], cssClass : 'theor1corollary theor2proof', },    // Bh
-            { nam : ['C', 'h'], cssClass : 'theor1corollary theor2proof', },    // Ch
-            { nam : ['E', 'g'], cssClass : 'theor1corollary', },                // Eg
-            { nam : ['F', 'g'], cssClass : 'theor1corollary', },                // Fg
 
             { nam : ['D', 'e'], cssClass : 'theor1proof theor2proof', },        // De
             { nam : ['E', 'f'], cssClass : 'theor1proof theor2proof', },        // Ef
@@ -377,7 +342,7 @@
             { nam : ['C','Paracc'], cssClass : 'theor2corollary', },        // CParacc
             { nam : ['V','Varacc'], cssClass : 'theor2corollary', },        // VVaracc
             { nam : ['B','Varacc'], cssClass : 'theor2corollary', },        // BVaracc
-            { nam : ['Caracc','Paracc'], cssClass : 'theor2corollary', },
+            { nam : ['Caracc','Paracc'], cssClass : 'theor2corollary', },   // CaraccParacc
 
         ].forEach( pNam => {
             if( pNam.nam[0] === 'A' && pNam.nam[1] === 'v' ) {
@@ -448,7 +413,6 @@
         //      due transparency are still well-visible,
         saggPolyNames_2_rg8media();
         keplerPolyNames_2_rg8media();
-        arbitraryArea_PolyNames_2_rg8media();
         acceleratingArea_2_rg8media();
 
         toreg( 'displayTime' )( 'value', '' );
@@ -474,13 +438,13 @@
         if (sconf.TIMER_AND_LOGIC_STEPS_COINSIDE === false) {
             rg.SC.decStart = 7;
 
-            [ rg.D, rg.d, rg.Dd, rg.Cd, rg.CD, rg.SD, rg.Sd, rg.SCd, rg.SABCD ].forEach( pn => {
+            [ rg.D, rg.d, rg.Dd, rg.Cd, rg.CD, rg.SD, rg.Sd, rg.SCd ].forEach( pn => {
                 pn.decStart = rg.SC.decStart + 4;
             });
             [ rg.E, rg.e, rg.Ee, rg.De, rg.DE, rg.SE, rg.Se, rg.SDe ].forEach( pn => {
                 pn.decStart = rg.SC.decStart + 8;
             });
-            [ rg.F, rg.Z, rg.W, rg.EW, rg.g, rg.Eg, rg.Fg, rg.DF, rg.f, rg.Ff, rg.Ef, rg.EF, rg.SF, rg.Sf, rg.SEf, rg.SABCDEF ].forEach( pn => {
+            [ rg.F, rg.Z, rg.W, rg.EW, rg.DF, rg.f, rg.Ff, rg.Ef, rg.EF, rg.SF, rg.Sf, rg.SEf ].forEach( pn => {
                 pn.decStart = rg.SC.decStart + 12;
             });
         }
@@ -489,9 +453,9 @@
         //Update decEnd for the following decorations, to ensure they are hidden once the time slider is advanced beyond point F.
         [
             rg.c, rg.Cc, rg.Bc, rg.Sc, rg.SBc,
-            rg.d, rg.Dd, rg.Cd, rg.SD, rg.Sd, rg.SCd, rg.SABCD, 
+            rg.d, rg.Dd, rg.Cd, rg.SD, rg.Sd, rg.SCd, 
             rg.e, rg.Ee, rg.De, rg.SE, rg.Se, rg.SDe,
-            rg.f, rg.Ff, rg.Ef, rg.SF, rg.Sf, rg.SEf, rg.SABCDEF,
+            rg.f, rg.Ff, rg.Ef, rg.SF, rg.Sf, rg.SEf,
         ].forEach( pn => {
             pn.decEnd = rg.f.decStart + 3;
         });
@@ -505,8 +469,8 @@
     function saggPolyNames_2_rg8media(
     ){
         [
-            ['A', 'B', 'C', 'V'],
-            ['D', 'E', 'F', 'Z'],
+            ['A', 'B', 'C', 'V'],   //ABCV
+            ['D', 'E', 'F', 'Z'],   //DEFZ
         ].forEach( pNames => {
             var rgElem = ssF.pnames2poly(
                 pNames,
@@ -530,10 +494,10 @@
     ){
         [   ////these triangles override free triangles attached to path
             ////in path-2-media js-code,
-            ['S', 'B', 'c',],
-            ['S', 'C', 'd',],
-            ['S', 'D', 'e',],
-            ['S', 'E', 'f',],
+            ['S', 'B', 'c',],   //SBc
+            ['S', 'C', 'd',],   //SCd
+            ['S', 'D', 'e',],   //SDe
+            ['S', 'E', 'f',],   //SEf
         ].forEach( pNames => {
             var rgElem = ssF.pnames2poly(
                 pNames,
@@ -551,33 +515,13 @@
         ///makes first few free-triangles living
         ///until the passing of the point f
         [
-            ['S', 'B', 'c',],
-            ['S', 'C', 'd',],
-            ['S', 'D', 'e',],
-            ['S', 'E', 'f',],
+            ['S', 'B', 'c',],   //SBc
+            ['S', 'C', 'd',],   //SCd
+            ['S', 'D', 'e',],   //SDe
+            ['S', 'E', 'f',],   //SEf
         ].forEach( pNames => {
             var pn = pNames.join('');
             rg[pn].decEnd = rg.f.decEnd;
-        });
-    }
-
-    function arbitraryArea_PolyNames_2_rg8media(
-    ){
-        [
-            ['S', 'A', 'B', 'C', 'D' ],
-            ['S', 'A', 'B', 'C', 'D', 'E', 'F'],
-        ].forEach( pNames => {
-            var rgElem = ssF.pnames2poly(
-                pNames,
-                'theor1proof tofill',
-                null,
-                !!'undisplay',
-                !'tostroke',
-            );
-            var lp = rg[ pNames[ pNames.length-1 ] ];
-            decor[ rgElem.pname ] = rgElem;
-            rgElem.decStart = lp.decStart;
-            rgElem.decEnd = 4 * 1.2 * sconf.numberOfManyBases;
         });
     }
 
@@ -586,7 +530,7 @@
     function acceleratingArea_2_rg8media(
     ){
         [
-            ['S', 'B', 'Caracc',],
+            ['S', 'B', 'Caracc',],  //SBCaracc
         ].forEach( pNames => {
             var rgElem = ssF.pnames2poly(
                 pNames,
