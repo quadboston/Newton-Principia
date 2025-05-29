f7ef60ee469622febfcba41eee07eeca9d27c517
John Scott <j@sciencehike.com>
2025-05-28 21:51:27 -0600

    Update changelog

--- changelog/version.js
+++ changelog/version.js
@@ -3,8 +3,8 @@
     var fapp        = ns.fapp           = ns.fapp           || {};
 
     // //\\ updated automatically. Don't edit these strings.
-    fapp.version = 13654; //application version
-    fapp.buildDateString = "2025-05-20"; //build date
+    fapp.version = 13655; //application version
+    fapp.buildDateString = "2025-05-29"; //build date
     // \\// updated automatically. Don't edit these strings.
 
     }) ();
