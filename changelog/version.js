44d1c105fc6b9daf88bc18ee7a3d0841110ce897
John Scott <j@sciencehike.com>
2025-05-28 22:52:15 -0600

    Correct changelog back link for website

--- changelog/version.js
+++ changelog/version.js
@@ -3,7 +3,7 @@
     var fapp        = ns.fapp           = ns.fapp           || {};
 
     // //\\ updated automatically. Don't edit these strings.
-    fapp.version = 13657; //application version
+    fapp.version = 13658; //application version
     fapp.buildDateString = "2025-05-29"; //build date
     // \\// updated automatically. Don't edit these strings.
 
