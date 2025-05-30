48644af840839691fd9729e6d3a2785221ff9d28
John Scott <j@sciencehike.com>
2025-05-30 09:40:17 -0600

    Do not create addendum.src.html

--- changelog/version.js
+++ changelog/version.js
@@ -3,8 +3,8 @@
     var fapp        = ns.fapp           = ns.fapp           || {};
 
     // //\\ updated automatically. Don't edit these strings.
-    fapp.version = 13657; //application version
-    fapp.buildDateString = "2025-05-29"; //build date
+    fapp.version = 13658; //application version
+    fapp.buildDateString = "2025-05-30"; //build date
     // \\// updated automatically. Don't edit these strings.
 
     }) ();
