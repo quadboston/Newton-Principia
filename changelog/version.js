5e81afebfb932841d3b82a80cc44a55a33650587
Konstantin Kirillov <github@landkey.net>
2025-06-02 21:57:34 -0500

    CalculusXX to VC

--- changelog/version.js
+++ changelog/version.js
@@ -3,7 +3,7 @@
     var fapp        = ns.fapp           = ns.fapp           || {};
 
     // //\\ updated automatically. Don't edit these strings.
-    fapp.version = 13796; //application version
+    fapp.version = 13797; //application version
     fapp.buildDateString = "2025-06-02"; //build date
     // \\// updated automatically. Don't edit these strings.
 
