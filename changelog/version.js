df5e3060d979b12a594096c19caf2440448e593b
Konstantin Kirillov <github@landkey.net>
2025-07-11 01:30:14 -0500

    L20 usage fixes2

--- changelog/version.js
+++ changelog/version.js
@@ -3,7 +3,7 @@
     var fapp        = ns.fapp           = ns.fapp           || {};
 
     // //\\ updated automatically. Don't edit these strings.
-    fapp.version = 13847; //application version
+    fapp.version = 13848; //application version
     fapp.buildDateString = "2025-07-11"; //build date
     // \\// updated automatically. Don't edit these strings.
 
