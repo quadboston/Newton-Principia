4c5f4599c833e160b38a363d3eb44d9da3b76c35
John Scott <j@sciencehike.com>
2025-08-03 12:29:33 -0600

    L20: Use italics for lower-case labels

--- changelog/version.js
+++ changelog/version.js
@@ -3,8 +3,8 @@
     var fapp        = ns.fapp           = ns.fapp           || {};
 
     // //\\ updated automatically. Don't edit these strings.
-    fapp.version = 13849; //application version
-    fapp.buildDateString = "2025-07-31"; //build date
+    fapp.version = 13850; //application version
+    fapp.buildDateString = "2025-08-03"; //build date
     // \\// updated automatically. Don't edit these strings.
 
     fapp.versionColor = 'purple'; //color for version display
