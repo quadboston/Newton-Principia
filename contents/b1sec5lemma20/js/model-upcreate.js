0e1ea22ad3418fcaec8b63f2bd5e658006c6b664
Konstantin Kirillov <github@landkey.net>
2025-08-03 12:41:17 -0500

    L20 no hyperbola asymptota

--- contents/b1sec5lemma20/js/model-upcreate.js
+++ contents/b1sec5lemma20/js/model-upcreate.js
@@ -1,6 +1,6 @@
 ( function() {
     var {
-        sn, mat,
+        sn, haz, mat,
         stdMod, rg, sconf, ssF, sData,
     } = window.b$l.apptree({
         stdModExportList :
@@ -225,14 +225,36 @@
     ///ellipse to array
     function ellmod2arr( arg )
     {
-        const pe = mat.polar_ellipse;
-        const points = arg.points = [];
         const stepsCount = arg.stepsCount;
         const step = 2*Math.PI/stepsCount;
+        const pe = mat.polar_ellipse;
+        const brs = sn( 'branches', arg, [] );
+        const points = arg.points = [];
+        let bpoints;
+        let ro;
+        let branchesLen = 0;
         for( var ii = 0; ii < stepsCount; ii++ ){
             arg.q = step * ii;
-            points.push( pe(arg).point );
+            const orb = pe(arg);
+            if( !ii || ro * orb.ro < 0 ){
+                ro = orb.ro;
+                bpoints = [];
+                brs[ branchesLen ] = brs[ branchesLen ] || {};
+                brs[ branchesLen ].points = bpoints;
+                branchesLen++;
+            }
+            bpoints.push( orb.point );
+            points.push( orb.point );
         }
+        ///removes dom stuff in brs if leftover
+        for( ii=branchesLen; ii < brs.length; ii++ ){
+            const svgel = haz( brs[ii], 'svgel' );
+            if( svgel ){
+                svgel.remove();
+                delete brs[ii].svgel;
+            }
+        }
+        brs.length = branchesLen;
     };
 })();
 
