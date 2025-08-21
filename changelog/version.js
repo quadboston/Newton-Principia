14ca9d54894a8505c7193551aed6e7ddd1cf9dc0
Konstantin Kirillov <github@landkey.net>
2025-08-21 14:54:45 -0500

    doc added

--- changelog/version.js
+++ changelog/version.js
@@ -3,8 +3,8 @@
     var fapp        = ns.fapp           = ns.fapp           || {};
 
     // //\\ updated automatically. Don't edit these strings.
-    fapp.version = 13678; //application version
-    fapp.buildDateString = "2025-08-12"; //build date
+    fapp.version = 13681; //application version
+    fapp.buildDateString = "2025-08-19"; //build date
     // \\// updated automatically. Don't edit these strings.
 
     fapp.versionColor = 'purple'; //color for version display
