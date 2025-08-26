a33e59fbf76dbec0278e987869246b013ba43661
Konstantin Kirillov <github@landkey.net>
2025-08-26 10:46:45 -0500

    update till branch cssClass-fixes

--- contents/b1sec2prop10/js/amode8captures.js
+++ contents/b1sec2prop10/js/amode8captures.js
@@ -1,6 +1,6 @@
 ( function() {
-    var { ns, sn, nspaste, capture, toreg, sDomF, ssD, ssF, fconf, stdMod, amode, 
-        rg, sconf, } = window.b$l.apptree({ ssFExportList : { amode2rgstate, }, });
+    const { nspaste, capture, toreg, sDomF, ssF, stdMod, amode, rg, sconf, } =
+            window.b$l.apptree({ ssFExportList : { amode2rgstate, }, });
     setCapture();
     return;
 
@@ -32,15 +32,16 @@
         });
     }
 
-
     ///runs inside "subessay launch" which in turn runs after
     ///"init model parameters"
     function amode2rgstate( captured )
     {
-        var { logic_phase, aspect, subessay } = amode;
-        var media_scale = toreg( 'media_scale' )();
+        const { logic_phase, aspect, subessay } = amode;
+        const media_scale = toreg( 'media_scale' )();
         rg.media_scale.value = 1;
         ssF.scaleValue2app( rg.media_scale.value, stdMod );
+
+        //todo sort out why this is commented out,
         // toreg( 'sForSagitta' )( 'val', sconf.sForSagitta_valQ );
         // nspaste( rg.P.pos, rg[ 'approximated-curve' ].t2xy( sconf.PparT ));
 
@@ -51,15 +52,8 @@
         rg.S.pos[0] = 0;
         rg.S.pos[1] = 0;
 
-        var hideAnother = subessay !== 'another-solution';
-        rg.u.undisplay = hideAnother;
-        rg.Tu.undisplay = hideAnother;
-        rg[ 'u,VV' ].undisplay = hideAnother;
-        rg.uP.undisplay = hideAnother;
-        rg.PQ.undisplay = hideAnother;
-        rg.tangentCircle.undisplay = hideAnother;
-        rg[ 'P,tCircleCenter' ].undisplay = hideAnother;
-        rg[ 'tCircleCenter' ].undisplay = hideAnother;
+        rg.tangentCircle.undisplay = subessay !== 'another-solution';
+        rg.Q.hideD8Dpoint = logic_phase !== 'proof';
 
         //=====================================================
         // //\\ alternates for addendum
@@ -68,42 +62,6 @@
         rg.S.d8d_find_is_LOCKED = rg.S.hideD8Dpoint = !isAdden;
         rg.S.undisplayAlways = !isAdden;
         rg.S.doPaintPname = isAdden;
-
-        //rg.PF.undisplay = isAdden;
-        //rg.GP.undisplay = isAdden;
-        //rg.G.undisplay = isAdden;
-        //rg.DK.undisplay = isAdden;
-        //rg.K.undisplay = isAdden;
-        //rg.D.undisplay = isAdden;
-        rg.F.undisplay = isAdden;
-        rg.A.undisplay = isAdden;
-        rg.B.undisplay = isAdden;
-        //rg.G.undisplay = isAdden;
-        //rg.T.undisplay = isAdden;
-        rg.Q.undisplay = isAdden;
-        rg.R.undisplay = isAdden;
-
-        rg.VV.undisplay = isAdden;
-        rg.v.undisplay = isAdden;
-        rg.u.undisplay = isAdden;
-        rg.Pv.undisplay = isAdden;
-        rg.Qv.undisplay = isAdden;
-        //rg.Tv.undisplay = isAdden;
-        rg.vG.undisplay = isAdden;
-        rg.QT.undisplay = isAdden;
-        rg.PT.undisplay = isAdden;
-        rg.QR.undisplay = isAdden;
-        rg.SQ.undisplay = isAdden;
-        rg.GO.undisplay = isAdden;
-
-        rg.AO.undisplay = isAdden;
-        rg.DO.undisplay = isAdden;
-        rg.BO.undisplay = isAdden;
-        rg.PO.undisplay = isAdden;
-        rg.FO.undisplay = isAdden;
-        //rg[ 'P,VV' ].undisplay = isAdden;
-        //rg[ 'A,AA' ].undisplay = isAdden;
-        //rg[ 'B,BB' ].undisplay = isAdden;
         //=====================================================
         // \\// alternates for addendum
         //=====================================================
@@ -111,6 +69,4 @@
         sDomF.detected_user_interaction_effect( 'doUndetected' );
         return captured;
     }
-
-}) ();
-
+})();
