7b404fad2d89d09c17885fe25d0741bd2f5d3ea1
John Scott <j@sciencehike.com>
2026-05-20 15:35:00 -0600

    Zipify no longer changes version number

--- changelog/version.js
+++ changelog/version.js
@@ -2,10 +2,11 @@
     var ns          = window.b$l        = window.b$l        || {};
     var fapp        = ns.fapp           = ns.fapp           || {};
 
-    // //\\ updated automatically. Don't edit these strings.
-    fapp.version = 9; //application version
-    fapp.buildDateString = "2026-04-17"; //build date
-    // \\// updated automatically. Don't edit these strings.
+    fapp.version = '0.9.1'; //application version
+
+	// //\\ updated automatically. Don't edit this string.
+    fapp.buildDateString = "2026-05-20"; //build date
+    // \\// updated automatically. Don't edit this string.
 
     fapp.versionColor = 'Chartreuse'; //color for version display
 }) ();
--- contents/conf/home-page-conf.html.js
+++ contents/conf/home-page-conf.html.js
@@ -161,7 +161,7 @@
                         <span class="dd-label"><a href="https://www.linkedin.com/in/kathryn-lepome">Kathryn LePome:</a></span>
                             voice<br>
                         <span class="dd-label"><a href="https://github.com/quadboston/Newton-Principia">Source Code:</a></span> 
-                            open source, MIT license. Built ${fapp.buildDateString}, <span style="color: ${fapp.versionColor}">Version 0.${fapp.version}</span><br>
+                            open source, MIT license. Built ${fapp.buildDateString}, <span style="color: ${fapp.versionColor}">Version ${fapp.version}</span><br>
                         <span class="dd-label"><a href="changelog/changelog.html">What's New</a></span> 
                     </p>
                 </div>`;
